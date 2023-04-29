c1d58518778987faaaaaba780fb1e41b2dd930e1
Saad Elkammah <tamer12345sdf@gmail.com>
2023-04-29 20:18:11 +0200

    import from the Excel file with mapping
    import from the Excel file with mapping

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -34,6 +34,20 @@ class ProductController extends Controller
             $name_index = array_search('name', $row);
             $quantity_index = array_search('quantity', $row);
             $type_index = array_search('type', $row);
+
+            // If any of the expected columns are not found, attempt to map them
+            if ($name_index === false || $quantity_index === false || $type_index === false) {
+                for ($i = 0; $i < count($row); $i++) {
+                    if ($name_index === null && preg_match('/name/i', $row[$i])) {
+                        $name_index = $i;
+                    } elseif ($quantity_index === null && preg_match('/quantity/i', $row[$i])) {
+                        $quantity_index = $i;
+                    } elseif ($type_index === null && preg_match('/type/i', $row[$i])) {
+                        $type_index = $i;
+                    }
+                }
+            }
+
             continue; // skip the header row
         }
 
